5ab2f67ef7752c2e21bafd344bdc97cfd91ee8a1
jz@macOS <saumoon@gmail.com>
2023-08-05 21:53:54 +0800

    First commit

---
+++ firmware/3x5_3/rules.mk
@@ -0,0 +1,4 @@
+SERIAL_DRIVER = vendor
+
+CUSTOM_MATRIX = yes
+SRC += matrix.c
---
+++ firmware/keymaps/3x5_3_fancy/rules.mk
@@ -0,0 +1,7 @@
+# TAP_DANCE_ENABLE = yes      # Enable Tap-Dance feature
+# COMBO_ENABLE = yes          # Enable COMBO feature
+
+ENCODER_ENABLE = yes
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3360
